801609efca48995f850362e1251a4ccba3c0d17f
oretaka <oretaka21@gmail.com>
2018-11-08 12:02:19 +0900

    gotoDetail

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   // DAOクラスの読み込み
   require_once("backphp/Dao.class.php");
 
-  //    ここから　変数宣言・初期化
+  //   										ここから　変数宣言・初期化
   
   // 現在ログイン中のユーザーID。
   $id = -1;
@@ -26,68 +26,124 @@
   // 結婚式の開始時間を、ユーザーに分かりやすい形式に変換したテキスト。
   $startTimeShow = '';
 
-  //    ここまで　変数宣言・初期化
+  // 		for文による結婚式募集情報一覧生成に関わる変数
+  // 一時的に格納する男の名前
+  $maleName = "";
+  // 一時的に格納する女の名前
+  $femaleName = "";
+  // 結婚式の日付
+  $date = "";
+  // 結婚式の時間
+  $time = "";
+  // 結婚式の備考
+  $note = "";
+  // 募集中の結婚式id
+  $weddingID = 0;
 
-  // 最新の結婚式情報の取得を行う。
-  $dbres = $dao->select("wedding_register",null,null,null,null,"ORDER BY date, time");
-  // 取得した結婚式の情報数分、HTMLを生成し、BridalParty欄に表示させる(横は最大2件表示)。
-  for($i=0;$i<count($dbres);$i++)
+  //   										ここまで　変数宣言・初期化
+
+  // 結婚式情報の取得処理のみの場合
+  if( isset($_POST["wnum"]) )
   {
-    if($i % 2 == 0)
+    // 押されたボタンに該当する結婚式情報を、json形式で返す。
+    $wnum = intval($_POST["wnum"]);
+    $rs = array(
+      "weddingID" => $dbres[$wnum]["weddingID"],
+      "maleName" => $dbres[$wnum]["maleName"],
+      "femaleName" => $dbres[$wnum]["femaleName"],
+      "date" => $dbres[$wnum]["date"],
+      "time" => $dbres[$wnum]["time"],
+      "note" => $dbres[$wnum]["note"]
+    );
+    $jsonstr =  json_encode($rs, JSON_UNESCAPED_UNICODE);
+    print_r($jsonstr);
+    exit;
+  }
+  else
+  {
+    // 最新の結婚式情報の取得を行う。
+    $dbres = $dao->select("wedding_register",null,null,null,null,"ORDER BY date, time");
+    // 取得した結婚式の情報数分、HTMLを生成し、BridalParty欄に表示させる(横は最大2件表示)。
+    for($i=0;$i<count($dbres);$i++)
     {
-      // $iが偶数
-      // profileHolderの開始
-      $bpHtml .= '<div class="profileHolder">';
-    }
+      // データベースから、結婚式の募集情報を随時取得する。
+      $maleName = $dbres[$i]["maleName"];
+      $femaleName = $dbres[$i]["femaleName"];
+      $date = date("Y/m/j", strtotime( $dbres[$i]["date"] ));
+      $time = date("H:i～", strtotime( $dbres[$i]["time"] ));
+      $note = $dbres[$i]["note"];
 
-    $bpHtml .= '<div class="width50"><img class="profileImage" src="img/userimg.jpg">';
+      if($i % 2 == 0)
+      {
+        // $iが偶数
+        // profileHolderの開始
+        $bpHtml .= '<div class="profileHolder">';
+      }
 
-    // 結婚式のタイトル(相手の名前・自分の名前を表示させます)
-    $bpHtml .= '<h3>' . $dbres[$i]["maleName"] . '　♡　' . $dbres[$i]["femaleName"] . '</h3>';
+      $bpHtml .= '<div class="width50"><img class="profileImage" src="img/userimg.jpg">';
 
-    // 結婚式の時間(開始時間のみ)・備考など
-    $bpHtml .= '<p>';
-    // 時間
-    // DB上の時間を、ユーザーが分かりやすい表示形式に変換する。
-    $startTimeShow =  date("Y/m/j", strtotime( $dbres[$i]["date"] )) . "　" . date("H:i～", strtotime( $dbres[$i]["time"] ));
-    $bpHtml .= '<b>' . $startTimeShow .  '</b><br>';
+      // hiddenタグで、結婚式の募集情報をセットする。
+      $bpHtml .= '<form action="detail.php" method="post">';
+      $bpHtml .= '<input type="hidden" name="weddingID" value="' . $dbres[$i]["weddingID"] . '">';
+      $bpHtml .= '<input type="hidden" name="maleName" value="' . $maleName . '">';
+      $bpHtml .= '<input type="hidden" name="femaleName" value="' . $femaleName . '">';
+      $bpHtml .= '<input type="hidden" name="date" value="' . $date . '">';
+      $bpHtml .= '<input type="hidden" name="time" value="' . $time . '">';
+      $bpHtml .= '<input type="hidden" name="note" value="' . $note . '">';
 
-    // 備考欄
-    $bpHtml .= $dbres[$i]["note"];
-    $bpHtml .= '</p>';
-    $bpHtml .= '</div>';
+      // 結婚式のタイトル(相手の名前・自分の名前を表示させます)
+      $bpHtml .= '<h3>' . $maleName . '　♡　' . $femaleName . '</h3>';
 
-    if($i % 2 == 1)
-    {
-      // $iが奇数
-      // profileHolderの終了
+      // 結婚式の時間(開始時間のみ)・備考など
+      $bpHtml .= '<p>';
+      // 時間
+      // DB上の時間を、ユーザーが分かりやすい表示形式に変換する。
+      $startTimeShow =  $date . "　" . $time;
+      $bpHtml .= '<b>' . $startTimeShow .  '</b><br>';
+
+      // 備考欄
+      $bpHtml .= $note;
+      $bpHtml .= '</p>';
+
+      // 詳細ページに飛ばすリンクボタンの設置
+      $bpHtml .= '<input type="submit" value="詳細を見る">';
+      $bpHtml .= '</form>';
       $bpHtml .= '</div>';
+
+      if($i % 2 == 1)
+      {
+        // $iが奇数
+        // profileHolderの終了
+        $bpHtml .= '</div>';
+      }
     }
-  }
 
 
 
-  // セッションからユーザーID・ニックネームが取得できる場合は取得し、ログイン中の表示を行う。
-  if( isset($_SESSION["id"]) )
-  {
-    $id = intval( $_SESSION["id"] );
-    $nickname = $_SESSION["nickname"];
-    $logout_but = "<a href=\"backphp/logout.php\">ログアウト</a>";
-  }
-  // 
-  // GETリクエストからメッセージコードを取得できる場合は取得する。
-  if( isset($_GET["result"]) )
-  {
-    $result = intval($_GET["result"]);
-    $msg = "<p>";
-    switch($result)
+    // セッションからユーザーID・ニックネームが取得できる場合は取得し、ログイン中の表示を行う。
+    if( isset($_SESSION["id"]) )
     {
-      case 100:
-        $msg .= "結婚式の募集が完了しました。";
-        break;
+      $id = intval( $_SESSION["id"] );
+      $nickname = $_SESSION["nickname"];
+      $logout_but = "<a href=\"backphp/logout.php\">ログアウト</a>";
     }
-    $msg .= "</p>";
+    // 
+    // GETリクエストからメッセージコードを取得できる場合は取得する。
+    if( isset($_GET["result"]) )
+    {
+      $result = intval($_GET["result"]);
+      $msg = "<p>";
+      switch($result)
+      {
+        case 100:
+          $msg .= "結婚式の募集が完了しました。";
+          break;
+      }
+      $msg .= "</p>";
+    }
   }
+
+  
 
 ?>
 
@@ -108,6 +164,22 @@
 </head>
 
 <body>
+
+  <script>
+
+    function gotoDetail(num)
+    {
+      // ajaxで押されたボタンの結婚式情報を取得。
+      $.post("index.php",
+        {wnum: num},
+        function(data){
+          //リクエストが成功した際に実行する関数
+          alert(data);
+        }
+      );
+    }
+
+  </script>
 
   <div class="homepage">
   <div class="leftHalf">
@@ -204,9 +276,17 @@
   
    <div class="section" id="bridalParty">
       <div class="contentArea">
+      	<!-- <form action="detail.php" method="post"> -->
         <h2>Bridal Party</h2>
         <p>Schedule list</p>
         <?php echo $bpHtml; ?>
+        <!-- <input type="hidden" name="maleName" id="maleName" value="">
+        <input type="hidden" name="femaleName" id="femaleName" value="">
+        <input type="hidden" name="date" id="date" value="">
+        <input type="hidden" name="time" id="time" value="">
+        <input type="hidden" name="note" id="note" value="">
+        <input type="hidden" name="weddingID" id="weddingID" value=""> -->
+
         
         <!-- <div class="profileHolder">
           <div class="width50">
@@ -250,8 +330,9 @@
            </div>   
           </div> -->
 <!--   profile holder end      -->
+
+		<!-- </form> -->
        
-   
      </div>
 <!--   Content Area End    -->
   </div>
